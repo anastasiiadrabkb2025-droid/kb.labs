f6c49ad257505e10771627737877841184a7a757
anastasiiadrabkb2025-droid <anastasiia.drab.kb.2025@lpnu.ua>
2026-09-21 10:57:34 +0300

    Update main.py

--- labs/lab01/main.py
+++ labs/lab01/main.py
@@ -1,12 +1,12 @@
 
 
-from task1 import analyze_passwords
+from task1 import run_task1
 from task2 import run_task2
 from task3 import main as run_task3
 
 
 def main():
-    analyze_passwords()
+    run_task1()
     run_task2()
     run_task3()
 
